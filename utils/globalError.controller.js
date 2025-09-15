7d1ebda57c8ea8b13dbc96b3fa17f1146935a822
Niraj Shah <nirajshah950@gmail.com>
2025-09-15 23:12:51 +0530

    Global Error handler Function

---
+++ utils/globalError.controller.js
@@ -0,0 +1,9 @@
+let asyncErrorHandler = (func) => {
+  return (req, res, next) => {
+    func(req, res, next).catch((err) => {
+      next(err);
+    });
+  };
+};
+
+module.exports = asyncErrorHandler;
