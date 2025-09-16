82cf495d7ab1d722d9d4eba7cf65c733b22e41f0
Niraj Shah <nirajshah950@gmail.com>
2025-09-16 23:30:48 +0530

    Added route to test the status of the server.

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express");
-const { setupMiddlewares } = require("./utils/middleware");
-const { globalErrorHandler } = require("./utils/globalError.controller");
+const globalErrorHandler = require("./utils/globalError.controller");
 const { uploadRouter } = require("./upload/upload.route");
-const asyncErrorHandler = require("./utils/globalError.controller");
+const { setupMiddlewares } = require("./utils/middleware");
 
 const app = express();
+
 setupMiddlewares(app);
+
+app.get("/status", (req, res) => {
+  return res.status(200).json({
+    status: "success",
+  });
+});
 
 app.use("/app/v1/file", uploadRouter);
 
-app.use(asyncErrorHandler);
+// Global error handler (should be the last middleware)
+app.use(globalErrorHandler);
+
 module.exports = { app };
