455876652a5e8fb7ef0f86615f1bf450941d8da0
Niraj Shah <nirajshah950@gmail.com>
2025-09-18 22:47:17 +0530

    NEW defined the service functions for the chat.

---
+++ chat/chat.service.js
@@ -0,0 +1,14 @@
+const sendMessages = async ({ data }) => {};
+
+const deleteMessage = async ({ data }) => {};
+
+const listHistory = async ({ data }) => {};
+
+const deleteChat = async ({ data }) => {};
+
+module.exports = {
+  sendMessages,
+  deleteMessage,
+  listHistory,
+  deleteChat,
+};
