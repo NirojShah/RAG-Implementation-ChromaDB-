39b99f596d1ac292dfd25964bd67f354ed95c5a3
Niraj Shah <nirajshah950@gmail.com>
2025-09-22 23:19:57 +0530

    NEW
    1. mychat : to fetch all the chats.
    2. chat_upload_file: to upload the file for particular chat.

--- chat/chat.controller.js
+++ chat/chat.controller.js
@@ -1,16 +1,75 @@
+const { uploadHandler } = require("../upload/upload.service");
 const asyncErrorHandler = require("../utils/globalError.controller");
+const {
+  createChat,
+  listOfMyChat,
+  processGetChat,
+  updateChatFileInfo,
+} = require("./chat.service");
+const { newChatSchema } = require("./chat.validation");
 
-const getChat = asyncErrorHandler(async () => {});
+const chat = asyncErrorHandler(async (req, res) => {
+  const payload = newChatSchema.parse({
+    ...req.body,
+    user_id: req.user.user_id,
+  });
 
-const chat = asyncErrorHandler(async () => {});
+  const newchat = createChat(payload);
+
+  res.status(200).json({
+    status: "success",
+    message: "Chat created successfully",
+    data: payload,
+  });
+});
+
+const getChat = asyncErrorHandler(async (req, res) => {
+  const resp = await listOfMyChat({ user_id: req.user.user_id });
+  return res.status(200).json({
+    status: "success",
+    chats: resp,
+  });
+});
 
 const deleteChat = asyncErrorHandler(async () => {});
 
-const history = asyncErrorHandler(async () => {});
+const history = asyncErrorHandler(async (req, res) => {
+  return res.staus(200).json({
+    status: "success",
+    chat: resp,
+  });
+});
+
+const mychat = asyncErrorHandler(async (req, res) => {
+  const chat_id = req.params.chat_id;
+  console.log(chat_id);
+  const resp = await processGetChat({ chat_id });
+
+  return res.status(200).json({
+    status: "success",
+    chats: resp,
+  });
+});
+
+const chat_upload_file = asyncErrorHandler(async (req, res) => {
+  const file = req.files.file;
+  const chat_id = req.body.chat_id;
+  const user_id = req.user.user_id;
+  const uplaod = await uploadHandler({ file, user_id });
+  const resp = await updateChatFileInfo({ file_id: uplaod.file_id, chat_id });
+  return res.status(200).json({
+    status: "success",
+    ...resp,
+  });
+
+  // const resp = await
+});
 
 module.exports = {
   getChat,
   chat,
   deleteChat,
   history,
+  mychat,
+  chat_upload_file,
 };
