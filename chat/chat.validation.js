ec696a8049775e8d9e135e2c4cad3ad47cd7c8dd
Niraj Shah <nirajshah950@gmail.com>
2025-10-04 23:31:16 +0530

    made the file optional.

--- chat/chat.validation.js
+++ chat/chat.validation.js
@@ -3,6 +3,7 @@ const z = require("zod");
 const newChatSchema = z.object({
   chat_name: z.string().min(1, "Chat name is required"),
   user_id: z.string().min(1, "User ID is required"),
+  file: z.file().optional()
 });
 
 module.exports = {
