2c0dd7244a8f633952f11129806e1047780fca5d
Niroj Shah <105719758+NirojShah@users.noreply.github.com>
2025-12-27 22:51:01 +0530

    Clean up app.js by removing extra whitespace
    Removed unnecessary whitespace and adjusted formatting.

--- app.js
+++ app.js
@@ -8,20 +8,16 @@ const { chatRouter } = require("./chat/chat.route");
 const {messageRouter} = require("./message/message.route")
 
 const app = express();
-
 setupMiddlewares(app);
-
 app.get("/status", (req, res) => {
   return res.status(200).json({
     status: "success",
   });
 });
-
 app.use("/app/v1/user", userRouter);
 app.use("/app/v1/file", authenticateUser, uploadRouter);
 app.use("/app/v1/chat", authenticateUser, chatRouter);
 app.use("/app/v1/message",authenticateUser, messageRouter)
 
 app.use(globalErrorHandler);
-
 module.exports = { app };
